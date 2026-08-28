d43a65cedbd1a0d0b024e998526c09ea464bfce3
steniovm <steniovm@gmail.com>
2026-08-28 19:31:52 -0300

    creat screen

--- script.js
+++ script.js
@@ -6,11 +6,19 @@ const datafich = document.getElementById("datafich");
 const newfichButton = document.getElementById("newfich");
 const loadfichButton = document.getElementById("loadfich");
 const findfichButton = document.getElementById("findfich");
+const backButton = document.getElementsByClassName("backButton");
 
 newfichButton.addEventListener("click", () => {
   startscreem.classList.add("hidden");
   setupfich.classList.remove("hidden");
 });
+
+for (let i = 0; i < backButton.length; i++) {
+  backButton[i].addEventListener("click", () => {
+    setupfich.classList.add("hidden");
+    startscreem.classList.remove("hidden");
+  });
+}
 
 loadfichButton.addEventListener("click", () => {
   startscreem.classList.add("hidden");
